e715e076a5bcd75692ec86af64054508a59a5424
TamaoSaridzawa <tamaosaridzawa9@gmail.com>
2021-11-15 17:13:16 +0300

    Перемешать массив

--- CSJuniorClon/Program.cs
+++ CSJuniorClon/Program.cs
@@ -10,6 +10,33 @@ namespace CSJuniorClon
     {
         static void Main(string[] args)
         {
+            Random random = new Random();
+            int[] array = { 2, 5, 1, 7, 9, 1, 4, 8 };
+
+            array = ShuffleArray(array, random);
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+
+            Console.ReadKey();
+        }
+
+        static int[] ShuffleArray(int[] array, Random random)
+        {
+            int tempNumber;
+            int tempIndex;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                tempIndex = random.Next(i + 1);
+                tempNumber = array[tempIndex];
+                array[tempIndex] = array[i];
+                array[i] = tempNumber;
+            }
+
+            return array;
         }
     }
 }
