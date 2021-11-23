b24c352514253af537bfd1933382b6aa02c843c5
TamaoSaridzawa <tamaosaridzawa9@gmail.com>
2021-11-24 00:00:29 +0300

    Магазин ООП

--- Shop/Program.cs
+++ Shop/Program.cs
@@ -10,23 +10,43 @@ namespace Shop
     {
         static void Main(string[] args)
         {
-            Seller seller = new Seller("dad", 4 );
-            Player player = new Player("rbs", 50);
-            seller.ShowInfo();
-            Console.WriteLine();
-            player.ShowInfo();
+            Player player = new Player("Job", 1400);
+            Seller seller = new Seller();
+            bool isDeal = true;
 
-            seller.Sell(ref player);
-            seller.ShowInfo();
-            Console.ReadKey();
+            Console.WriteLine("Нажмите :");
+            Console.WriteLine($"1 - открытие своего инвентаря\n2 - обзор товара продавца\n3 - покупка товара\n4 - выход из магазина");
 
-            
+            while (isDeal)
+            {
+                Console.Write("Введите команду: ");
+                string userInput = Console.ReadLine();
+
+                switch (userInput)
+                {
+                    case "1":
+                        player.ShowItems();
+                        break;
+                    case "2":
+                        seller.ShowItems();
+                        break;
+                    case "3":
+                        seller.Sell(ref player);
+                        break;
+                    case "4":
+                        isDeal = false;
+                        break;
+                    default:
+                        Console.WriteLine("Введены некорректные данные");
+                        break;
+                }
+            }
         }
     }
 
     class Product
     {
-        public string Name;
+        public string Name { get; private set; }
         public int Price { get; private set; }
 
         public Product(string name, int price)
@@ -36,53 +56,118 @@ namespace Shop
         }
     }
 
-    class Character
+    class Player : Inform
     {
-        protected List<Product> _products = new List<Product>();
-        protected string _name;
-        protected int _money;
+        private List<Product> _items = new List<Product>();
 
-        public Character( string name, int money)
+        public string Name { get; private set; }
+        public int Money { get; private set; }
+
+        public Player(string name, int money) 
         {
-            _name = name;
-            _money = money;
+            Name = name;
+            Money = money;
         }
 
-        public void ShowInfo()
+        public void Pay(int price)
         {
-            foreach (var item in _products)
+            Money -= price;
+        }
+
+        public void PickItem(Product product)
+        {
+            _items.Add(product);
+        }
+
+        public void ShowItems()
+        {
+            Console.WriteLine($"Денег в кармане - {Money}");
+
+            if (_items.Count <= 0)
             {
-                Console.WriteLine(item.Name);
+                Console.WriteLine("В рюкзаке пусто ^_^");
+            }
+            else
+            {
+                Console.WriteLine("Вещи в рюкзаке:");
+
+                foreach (var item in _items)
+                {
+                    Console.WriteLine($"{item.Name}");
+                }
+            }   
+        }
+    }
+
+    class Seller : Inform
+    {
+        private List<Product> _products = new List<Product>();
+
+        public Seller()
+        {
+            ExposeProducts();
+        }
+
+        public void Sell(ref Player player)
+        {
+            Console.Write("Какой товар вам продать? ");
+            string userInput = Console.ReadLine();
+
+            if (FindProduct(userInput, out Product product))
+            {
+                if (player.Money >= product.Price)
+                {
+                    player.Pay(product.Price);
+
+                    player.PickItem(product);
+
+                    Console.WriteLine("Поздравляю с покупкой!");
+
+                    _products.Remove(product);
+                }
+                else
+                {
+                    Console.WriteLine("У вас недостаточно средств");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Такого товара у меня нет!");
             }
         }
 
-        public List<Product> GetProducts()
+        public void ShowItems()
         {
-            return _products;
-        }
-    }
+            if (_products.Count > 0)
+            {
+                Console.WriteLine("Сейчас у продавца есть следуюшие товары.");
 
-    class Player : Character
-    {
-        public Player(string name, int money) : base( name, money)
-        {
-
-        }
-
-        public void Add(Product product)
-        {
-            _products.Add(product);
-        }
-    }
-
-    class Seller : Character
-    {
-        public Seller(string name, int money) : base(name, money)
-        {
-            _products = AddProducts();
+                foreach (var item in _products)
+                {
+                    Console.WriteLine($"{item.Name}, цена - {item.Price}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Товара больше нет.");
+            }
         }
 
-        private List<Product> AddProducts()
+        private bool FindProduct(string userInput, out Product product)
+        {
+            for (int i = 0; i < _products.Count; i++)
+            {
+                if (userInput.ToLower() == _products[i].Name.ToLower())
+                {
+                    product = _products[i];
+                    return true;
+                } 
+            }
+            product = null;
+            return false;
+        }
+
+        private void ExposeProducts()
         {
             _products.Add(new Product("Хлеб", 30));
             _products.Add(new Product("Аптечка", 60));
@@ -90,14 +175,14 @@ namespace Shop
             _products.Add(new Product("Винтовка", 1200));
             _products.Add(new Product("Пистолет", 600));
             _products.Add(new Product("Дробовик", 800));
-
-            return _products;
-        }
-
-        public void Sell(ref Player player, List<Product> list)
-        {   
-            
-            _products.RemoveAt(0);
         }
     }
+
+    interface Inform
+    {
+       void ShowItems();
+    }
 }
+
+
+
