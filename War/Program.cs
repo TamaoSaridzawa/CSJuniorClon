9a8e89e341a3737f4817fe7c85a8eb1b959e277a
TamaoSaridzawa <tamaosaridzawa9@gmail.com>
2021-12-19 09:34:31 +0300

    Война армий #3

--- War/Program.cs
+++ War/Program.cs
@@ -30,7 +30,7 @@ namespace War
     {
         public string Name { get; protected set; }
         public int ChanceTriggeringSuperpowers { get; protected set; }
-        public string Specialization { get;protected set; }
+        public string Specialization { get; protected set; }
         public double Damage { get; protected set; }
         public double Health { get; protected set; }
         public double Armor { get; protected set; }
@@ -40,18 +40,21 @@ namespace War
             Name = name;
         }
 
-        public void ShowStats()
-        {
-            Console.WriteLine($"{Specialization}, Урон {Damage}, ЗДоровье {Health}, Броня {Armor}");
-        }
-
         public abstract void ChooseFriendlyTarget(List<Unit> units);
-
-        public abstract bool IgnoreTank();
 
         public abstract void DescribeAbility();
 
         public abstract  void UseSuperAbility(Unit unit, List<Unit> units);
+
+        virtual public bool IsIgnorTank()
+        {
+            return false;
+        }
+
+        public void ShowStats()
+        {
+            Console.WriteLine($"{Specialization}, Урон {Damage}, ЗДоровье {Health}, Броня {Armor}");
+        }
 
         public void GiveHealing(double health)
         {
@@ -64,7 +67,7 @@ namespace War
             Damage += damage;
         }
 
-        public bool CheckHealth()
+        public bool IsDied()
         {
             if (Health <= 0)
             {
@@ -124,17 +127,13 @@ namespace War
             Console.WriteLine($"С шансом {ChanceTriggeringSuperpowers}процентов,  повышает свою атаку на {_rabies} процентов");
         }
 
-        public override bool IgnoreTank()
-        {
-            return false;
-        }
-
-        public override void UseSuperAbility(Unit unit, List<Unit> units)
+        public override void UseSuperAbility(Unit enemyUnit, List<Unit> friendlyUnits)
         {
             Console.WriteLine($"{Name} ,{Specialization} Использовал 'бешенство, атака увеличена'");
 
             Damage += Damage / 100 * _rabies;
-            unit.TakeDamage(Damage);
+
+            enemyUnit.TakeDamage(Damage);
         }
     }
 
@@ -152,6 +151,11 @@ namespace War
             _aimedShot = Damage * 1.5;
         }
 
+        public override bool IsIgnorTank()
+        {
+            return true;
+        }
+
         public override void ChooseFriendlyTarget(List<Unit> units)
         {
             throw new NotImplementedException();
@@ -162,17 +166,12 @@ namespace War
             Console.WriteLine($"С шансом {ChanceTriggeringSuperpowers} наносит в полтора раза больше урона, Полностью игнорирует ЗАщитника вражесского отряда");
         }
 
-        public override bool IgnoreTank()
-        {
-            return true;
-        }
-
-        public override void UseSuperAbility(Unit unit, List<Unit> units)
+        public override void UseSuperAbility(Unit enemyUnit, List<Unit> friendlyUnits)
         {
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine($"{Name} ,{Specialization} Использовал 'Выстрел дуплетом' ");
 
-            unit.TakeDamage(_aimedShot);
+            enemyUnit.TakeDamage(_aimedShot);
             Console.ForegroundColor = ConsoleColor.Gray;
         }
     }
@@ -206,20 +205,15 @@ namespace War
             Console.WriteLine($"С шансом {ChanceTriggeringSuperpowers} исцеляет себя на {_firstAidKit} ед. здоровья, Так же увеличивает урон всей группы на {_battleCry} ед. Является лидером группы");
         }
 
-        public override bool IgnoreTank()
-        {
-            return false;
-        }
-
-        public override void UseSuperAbility(Unit unit, List<Unit> units)
+        public override void UseSuperAbility(Unit enemyUnit, List<Unit> friendlyUnits)
         {
             Console.WriteLine($"{Name} ,{Specialization} использовал аптечку, урон группы увеличен на {_battleCry}");
 
-            ChooseFriendlyTarget(units);
+            ChooseFriendlyTarget(friendlyUnits);
 
             Health += _firstAidKit;
 
-            unit.TakeDamage(Damage);
+            enemyUnit.TakeDamage(Damage);
         }
     }
 
@@ -245,21 +239,17 @@ namespace War
         public override void ChooseFriendlyTarget(List<Unit> units)
         {
             Random random = new Random();
+
             units[random.Next(0, units.Count)].GiveHealing(_healingBandage);
         }
 
-        public override void UseSuperAbility(Unit unit, List<Unit> units)
+        public override void UseSuperAbility(Unit enemyUnit, List<Unit> friendlyUnits)
         {
             Console.WriteLine($"{Name}, {Specialization} использовал исцелени");
 
-            ChooseFriendlyTarget(units);
+            ChooseFriendlyTarget(friendlyUnits);
 
-            unit.TakeDamage(Damage);
-        }
-
-        public override bool IgnoreTank()
-        {
-            return false;
+            enemyUnit.TakeDamage(Damage);
         }
     }
 
@@ -286,20 +276,15 @@ namespace War
             Console.WriteLine($"С шансом {ChanceTriggeringSuperpowers} наносит дополнительного урона зависящего от атаки противника, является танком группы и берет весь вражесский урон на себя");
         }
 
-        public override bool IgnoreTank()
+        public override void UseSuperAbility(Unit enemyUnit, List<Unit> friendlyUnits)
         {
-            return false;
-        }
-
-        public override void UseSuperAbility(Unit unit, List<Unit> units)
-        {
-            _spikedArmor = unit.Damage / 2;
+            _spikedArmor = enemyUnit.Damage / 2;
 
             Console.WriteLine($"{Name}, {Specialization} использовал 'Шипастый танк'");
 
-            unit.ReturnDamage(_spikedArmor);
+            enemyUnit.ReturnDamage(_spikedArmor);
 
-            unit.TakeDamage(Damage);
+            enemyUnit.TakeDamage(Damage);
         }
     }
 
@@ -312,7 +297,7 @@ namespace War
 
         public void Fight()
         {
-            while (_detachment.CheckForFighters() && _detachment2.CheckForFighters() )
+            while (_detachment.IsAlive() && _detachment2.IsAlive() )
             {
                 _detachment.JoinBattle(_detachment2.GetUnits(), _random);
 
@@ -323,11 +308,11 @@ namespace War
                 Console.WriteLine("---------------------------");
             }
 
-            if (!_detachment.CheckForFighters())
+            if (!_detachment.IsAlive())
             {
                 Console.WriteLine($"Армия {_detachment.Name} пала");
             }
-            else if (!_detachment2.CheckForFighters())
+            else if (!_detachment2.IsAlive())
             {
                 Console.WriteLine($"Армия {_detachment2.Name} пала");
             }
@@ -339,15 +324,51 @@ namespace War
         private List<Unit> _units = new List<Unit>();
         private int _size = 5;
         public string Name { get; private set; }
-       
+
+        public Detachment()
+        {
+            Create();
+        }
+
         public List<Unit> GetUnits()
         {
             return _units;
         }
 
-        public Detachment()
+        public void JoinBattle(List<Unit> units, Random random)
         {
-            Create();
+            Console.WriteLine($"Армия {Name} атакована");
+
+            for (int i = 0; i < units.Count; i++)
+            {
+                int indexUnit = random.Next(0, _units.Count());
+
+                int procent = random.Next(1, 101);
+
+                if (TryGetDefender() && !units[i].IsIgnorTank())
+                {
+                    units[i].Attack(GetDefender(out indexUnit), procent, units);
+                }
+                else
+                {
+                    units[i].Attack(_units[indexUnit], procent, units);
+                }
+
+                if (_units[indexUnit].IsDied())
+                {
+                    _units.RemoveAt(indexUnit);
+                }
+
+                if (_units.Count == 0)
+                {
+                    break;
+                }
+            }
+        }
+
+        public bool IsAlive()
+        {
+            return _units.Count > 0;
         }
 
         private void Create()
@@ -386,60 +407,31 @@ namespace War
             }
         }
 
-        public void JoinBattle(List<Unit> units, Random random)
-        {
-            Console.WriteLine($"Армия {Name} атакована");
-
-            for (int i = 0; i < units.Count; i++)
-            {  
-                    int indexUnit;
-
-                    int procent = random.Next(1, 101);
-
-                    if (DefenderisAlive(out indexUnit, random) && !units[i].IgnoreTank())
-                    {
-                        units[i].Attack(GetDefender(indexUnit), procent, units);
-                    }
-                    else
-                    {
-                        units[i].Attack(_units[indexUnit], procent, units);
-                    }
-
-                    if (_units[indexUnit].CheckHealth())
-                    {
-                        _units.RemoveAt(indexUnit);
-                    }
-
-                    if (_units.Count == 0)
-                    {
-                        break;
-                    }
-            }
-        }
-
-        public bool CheckForFighters()
-        {
-            return _units.Count > 0;
-        }
-
-        private Unit GetDefender(int indexDefender)
-        {
-            return _units[indexDefender];
-        }
-
-        private bool DefenderisAlive(out int indexDefender, Random random)
+        private Unit GetDefender(out int indexDefender)
         {
             for (int i = 0; i < _units.Count; i++)
             {
-                
                 if (_units[i].Specialization.Contains("Защитник"))
                 {
                     indexDefender = i;
+                    return _units[i];
+                }
+            }
+
+            indexDefender = -1;
+            return null;
+        }
+
+        private bool TryGetDefender()
+        {
+            foreach (var unit in _units)
+            {
+                if (unit.Specialization.Contains("Защитник"))
+                {
                     return true;
                 }
             }
 
-            indexDefender = random.Next(0, _units.Count);
             return false;
         }
     }
