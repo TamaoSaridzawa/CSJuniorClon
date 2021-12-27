30dbb1bbb2849e4d92892dc703e8828114e6cd45
TamaoSaridzawa <tamaosaridzawa9@gmail.com>
2021-12-28 00:25:55 +0300

    Объединение войск

---
+++ UnificationTroops/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnificationTroops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Soildier> soildiersOne = new List<Soildier> { new Soildier("Дэн"), new Soildier("Раф"), new Soildier("Бэн"), new Soildier("Боб") };
+            List<Soildier> soildiersTwo = new List<Soildier> { new Soildier("Зик"), new Soildier("Георг"), new Soildier("Барак"), new Soildier("Алекс") };
+
+            soildiersTwo = soildiersOne.Where(soildier => soildier.Name.ToUpper().StartsWith("Б")).Union(soildiersTwo).ToList();
+
+            foreach (var soildier in soildiersTwo)
+            {
+                Console.WriteLine(soildier.Name);
+            }
+
+            foreach (var item in soildiersOne)
+            {
+                Console.WriteLine(item.Name);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+
+    class Soildier
+    {
+        public string Name { get; private set; }
+
+        public Soildier(string name)
+        {
+            Name = name;
+        }
+    }
+}
